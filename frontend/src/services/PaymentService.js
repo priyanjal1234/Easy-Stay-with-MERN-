6b0541d3b871f770ed5b5c3f443a9941619e25cf
priyanjal1234 <priyanjal362@gmail.com>
2024-12-27 14:07:46 +0530

    Update PaymentService.js

--- frontend/src/services/PaymentService.js
+++ frontend/src/services/PaymentService.js
@@ -3,7 +3,7 @@ import api from "./api";
 class PaymentService {
   constructor() {
     this.api = api;
-    this.baseUrl = "http://localhost:3000/api/payment";
+    this.baseUrl = "https://easy-stay-with-mern-backend.onrender.com/api/payment";
   }
 
   async createPaymentIntent(amount) {
