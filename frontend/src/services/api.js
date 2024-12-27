aee85b3b1762551ef3687c3f3f5a0b61ae562377
priyanjal1234 <priyanjal362@gmail.com>
2024-12-27 14:07:01 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://easy-stay-with-mern-backend.onrender.com/api'
 })
 
 export default api
