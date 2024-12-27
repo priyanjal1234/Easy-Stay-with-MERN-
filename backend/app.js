0ccebbbcee3c90bc1c143c309734c0c1218dd2ac
priyanjal1234 <priyanjal362@gmail.com>
2024-12-27 14:06:11 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://easy-stay-with-mern-frontend.onrender.com',
     credentials: true
 }))
 app.use(session({
